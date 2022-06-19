431d39ff04f72970f976db34cb300091785e304e
Md Sayem Hossain <sayemawasome@gmail.com>
2022-06-19 09:52:14 +0600

    changed button border

--- src/app/modules/cv-generator/cv-maker-routing.module.ts
+++ src/app/modules/cv-generator/cv-maker-routing.module.ts
@@ -5,7 +5,7 @@ import { CvPdfPreviewComponent } from './cv-generator/cv-pdf-preview/cv-pdf-prev
 
 const routes: Routes = [
   { path: '', component: CvGeneratorComponent },
-  { path: 'preview', component: CvPdfPreviewComponent }
+   { path: 'preview', component: CvPdfPreviewComponent }
 ];
 
 @NgModule({
