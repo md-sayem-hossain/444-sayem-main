a04dd494dacf892375fc0fb936ed935a4e3250df
Md Sayem Hossain <sayemawasome@gmail.com>
2022-06-27 15:19:58 +0600

    clear cache issues

--- src/app/core/services/common.service.ts
+++ src/app/core/services/common.service.ts
@@ -251,6 +251,121 @@ export class CommonService {
       T3Von: '',
       T3Bis: '',
     }
+
+
+
+
+
+
+
+    // sayem=================
+    this.lehrStepOneData = {
+      textArea10: '',
+      textArea11: '',
+      textArea12: '',
+      textArea10PlaceHolder: 'Ich habe den Lebenslauf und das Motivationsschreiben mit der Seite Lehrstell.ch verfasst und meine IF-Lehrperson hat beides korrigiert. Mein älterer Bruder ist bereits in der Lehre und hat mein Bewerbungsdossier durchgeschaut und mir noch Tipps gegeben.',
+      textArea11PlaceHolder: 'Ich bin nicht der Klassenbeste, aber auch nicht schlecht in der Schule. Gute Noten habe ich im Englisch und im RZG, weil mich diese Fächer sehr interessieren. Im Fach Mathi und NT habe ich Mühe und muss viel lernen. Die Noten sind da nicht so gut, aber ich versuche noch besser zu werden.',
+      textArea12PlaceHolder: 'Meine Klassenlehrerin sagt, dass sie mein Verhalten gut findet. Ich versuche mich immer an die Schulhausregeln zu halten. Ich könnte jedoch im Unterricht noch selbständiger werden.',
+    }   
+    
+  
+  
+    this.lehrStepTwoData2 = {
+      textArea1: '',
+      textArea2: '',
+      textArea3: '',
+      textArea1PlaceHolder: 'Ich habe im Internet gesehen, dass Sie eine Lehrstelle als Logistiker mit Beginn im August 2022 anbieten. Ich bewerbe mich hiermit um diese Lehrstelle. Sehr gerne würde ich in Ihrem Unternehmen mit anpacken und die Ausbildung zum Logistiker EBA absolvieren.',
+      textArea2PlaceHolder: 'Der Beruf des Logistikers habe ich bereits beim Schnuppern bei der Firma Logik AG kennengelernt. Dort merkte ich, dass ich mich für eine Ausbildung als Logistiker bewerben werde, da mir die Schnuppertage in diesem Beruf sehr gefallen haben.',
+      textArea3PlaceHolder: 'Die Kontrolle und der Transport der Waren mit dem Gabelstapler bereiten mir grosse Freude. Auch der Kontakt mit Kunden gefällt mir. Ich arbeite gerne praktisch, packe mit an und erledige Aufträge konzentriert und zügig. Die Firma Logistik AG würde mir als Lehrbetrieb gefallen, da es ein familiäres Unternehmen ist.',
+    }
+  
+    this.schnStepTwoData2 = {
+      textArea1: '',
+      textArea2: '',
+      textArea3: '',
+      textArea1PlaceHolder: 'Ich habe im Internet gesehen, dass Sie eine Lehrstelle als Logistiker mit Beginn im August 2022 anbieten. Ich bewerbe mich hiermit um diese Lehrstelle. Sehr gerne würde ich in Ihrem Unternehmen mit anpacken und die Ausbildung zum Logistiker EBA absolvieren.',
+      textArea2PlaceHolder: 'Der Beruf des Logistikers habe ich bereits beim Schnuppern bei der Firma Logik AG kennengelernt. Dort merkte ich, dass ich mich für eine Ausbildung als Logistiker bewerben werde, da mir die Schnuppertage in diesem Beruf sehr gefallen haben.',
+      textArea3PlaceHolder: 'Die Kontrolle und der Transport der Waren mit dem Gabelstapler bereiten mir grosse Freude. Auch der Kontakt mit Kunden gefällt mir. Ich arbeite gerne praktisch, packe mit an und erledige Aufträge konzentriert und zügig. Die Firma Logistik AG würde mir als Lehrbetrieb gefallen, da es ein familiäres Unternehmen ist.',
+     }
+  
+  
+  
+  
+     this.lehrStepThreeData3 = {
+      textArea4: '',
+      textArea5: '',
+      textArea6: '',
+      textArea4PlaceHolder: 'Ich habe bereits bei Ihnen geschnuppert und es hat mir sehr gefallen. Ich möchte zu Ihnen, weil mir viele berichtet haben, dass die Lehrmeister sehr gut zu den Lehrlingen schauen und alle Lehrlinge, die ich kenne sehr gerne zur Arbeit kamen.',
+      textArea5PlaceHolder: 'Gehe auf die Website der Firma. Schaue nach, welche Produkte oder Dienstleistungen von der Firma angeboten werden und notiere dies in dieses Feld hier.',
+      textArea6PlaceHolder: 'Die Kontrolle und der Transport der Waren mit dem Gabelstapler bereiten mir grosse Freude. Auch der Kontakt mit Kunden gefällt mir. Ich arbeite gerne praktisch, packe mit an und erledige Aufträge konzentriert und zügig. Die Firma Logistik AG würde mir als Lehrbetrieb gefallen, da es ein familiäres Unternehmen ist.Ich habe bereits 4 Schnupperlehren gemacht: <br>  Positiv fand ich: Kundenkontakt, Team, Abwechslung. <br> Negativ fand ich: Bei zwei Betrieben durfte ich den ganzen Tag fast nur zusehen und nichts selber machen',
+    }
+  
+    this.schnStepThreeData3 = {
+      textArea4: '',
+      textArea5: '',
+      textArea6: '',
+      textArea4PlaceHolder: 'Ich habe bereits bei Ihnen geschnuppert und es hat mir sehr gefallen. Ich möchte zu Ihnen, weil mir viele berichtet haben, dass die Lehrmeister sehr gut zu den Lehrlingen schauen und alle Lehrlinge, die ich kenne sehr gerne zur Arbeit kamen.',
+      textArea5PlaceHolder: 'Gehe auf die Website der Firma. Schaue nach, welche Produkte oder Dienstleistungen von der Firma angeboten werden und notiere dies in dieses Feld hier.',
+      textArea6PlaceHolder: 'Die Kontrolle und der Transport der Waren mit dem Gabelstapler bereiten mir grosse Freude. Auch der Kontakt mit Kunden gefällt mir. Ich arbeite gerne praktisch, packe mit an und erledige Aufträge konzentriert und zügig. Die Firma Logistik AG würde mir als Lehrbetrieb gefallen, da es ein familiäres Unternehmen ist.Ich habe bereits 4 Schnupperlehren gemacht: <br>  Positiv fand ich: Kundenkontakt, Team, Abwechslung. <br> Negativ fand ich: Bei zwei Betrieben durfte ich den ganzen Tag fast nur zusehen und nichts selber machen',
+     }
+  
+  
+     this.lehrStepFourData4 = {
+      textArea7: '',
+      textArea8: '',
+      textArea9: '',
+      textArea7PlaceHolder: 'Dass ich gut betreut werde und gerne zur Arbeit kommen werde.',
+      textArea8PlaceHolder: 'Meine Stärken sind: Ich bin freundlich, hilfsbereit, pünktlich und fleissig. Meine Schwächen sind: Ich bin eher ruhig und zurückhaltend. Manchmal sage ich meine Meinung nicht.',
+      textArea9PlaceHolder: 'Hier zählst du gute Beispiele aus deinen Schnupperlehren auf.',
+    }
+  
+    this.schnStepFourData4 = {
+      textArea7: '',
+      textArea8: '',
+      textArea9: '',
+      textArea7PlaceHolder: 'Dass ich gut betreut werde und gerne zur Arbeit kommen werde.',
+      textArea8PlaceHolder: 'Meine Stärken sind: Ich bin freundlich, hilfsbereit, pünktlich und fleissig. Meine Schwächen sind: Ich bin eher ruhig und zurückhaltend. Manchmal sage ich meine Meinung nicht.',
+      textArea9PlaceHolder: 'Hier zählst du gute Beispiele aus deinen Schnupperlehren auf.',
+     }
+  
+  
+     this.lehrStepFiveData5 = {
+      textArea13: '',
+      textArea14: '',
+      textArea15: '',
+      textArea13PlaceHolder: 'Ich kann sehr gut im Team arbeiten. In der Schule haben wir im Projektunterricht oft in verschiedenen Gruppen gearbeitet und in der Freizeit spiele ich Basketball und dort ist Teamwork sehr wichtig.',
+      textArea14PlaceHolder: 'In der Primarschule konnte ich nicht gut streiten und wurde teilweise sehr wütend. Heute verhalte ich mich in Konfliktsituationen ruhig und überlege immer zuerst was ich mache oder sage.',
+      textArea15PlaceHolder: 'Ich spiele Basketball in der U15-Mannschaft im BSK Muster. In der Freizeit gehe ich gerne in die Stadt mit Freunden und in den Schulferien gehe oft mit meiner Familie nach Portugal, da ich ursprünglich von dort komme.',
+    }
+  
+    this.schnStepFiveData5 = {
+      textArea13: '',
+      textArea14: '',
+      textArea15: '',
+      textArea13PlaceHolder: 'Ich kann sehr gut im Team arbeiten. In der Schule haben wir im Projektunterricht oft in verschiedenen Gruppen gearbeitet und in der Freizeit spiele ich Basketball und dort ist Teamwork sehr wichtig.',
+      textArea14PlaceHolder: 'In der Primarschule konnte ich nicht gut streiten und wurde teilweise sehr wütend. Heute verhalte ich mich in Konfliktsituationen ruhig und überlege immer zuerst was ich mache oder sage.',
+      textArea15PlaceHolder: 'Ich spiele Basketball in der U15-Mannschaft im BSK Muster. In der Freizeit gehe ich gerne in die Stadt mit Freunden und in den Schulferien gehe oft mit meiner Familie nach Portugal, da ich ursprünglich von dort komme.',
+    }
+  
+  
+  
+    this.lehrStepSixData6 = {
+      textArea16: '',
+      textArea17: '',
+      textArea18: '',
+      textArea16PlaceHolder: 'Mein Vater arbeitet bei der Firma Muster AG und meine Mutter arbeitet drei Mal pro Woche in der Pflege. Ich habe keine Geschwister und meine Eltern unterstützen mich in der Schule.',
+      textArea17PlaceHolder: 'Ich werde mit dem ÖV zur Arbeit fahren. Später möchte ich mir eventuell noch einen Roller kaufen, damit ich an schönen Tagen mit dem Roller zur Arbeit fahren kann.',
+      textArea18PlaceHolder: 'Ich möchte meine Lehre erfolgreich abschliessen und dann viele Weiterbildungen machen und erfolgreich im Beruf sein. Ich möchte einmal eine eigenen Familie haben und in einer schönen Wohnung wohnen. ',
+    }
+  
+    this.schnStepSixData6 = {
+      textArea16: '',
+      textArea17: '',
+      textArea18: '',
+      textArea16PlaceHolder: 'Mein Vater arbeitet bei der Firma Muster AG und meine Mutter arbeitet drei Mal pro Woche in der Pflege. Ich habe keine Geschwister und meine Eltern unterstützen mich in der Schule.',
+      textArea17PlaceHolder: 'Ich werde mit dem ÖV zur Arbeit fahren. Später möchte ich mir eventuell noch einen Roller kaufen, damit ich an schönen Tagen mit dem Roller zur Arbeit fahren kann.',
+      textArea18PlaceHolder: 'Ich möchte meine Lehre erfolgreich abschliessen und dann viele Weiterbildungen machen und erfolgreich im Beruf sein. Ich möchte einmal eine eigenen Familie haben und in einer schönen Wohnung wohnen. ',
+    }
   }
 
   setLocalStorgeDataForCV() {
