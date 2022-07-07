3c2b052462e941e94b68bee10d64aa07026f14f7
Md Sayem Hossain <sayemawasome@gmail.com>
2022-07-07 10:31:57 +0600

    schn cover word upadte

--- src/app/modules/motivation-sschreiben/ms-schn-preview/ms-schn-preview.component.ts
+++ src/app/modules/motivation-sschreiben/ms-schn-preview/ms-schn-preview.component.ts
@@ -4,6 +4,18 @@ import html2canvas from 'html2canvas';
 import { CommonService } from 'src/app/core/services/common.service';
 import { Router } from '@angular/router';
 import { DomSanitizer } from "@angular/platform-browser";
+import {
+  Document,
+  Packer,
+  Paragraph,
+  TextRun,
+  HeadingLevel,
+  BorderStyle,
+  ImageRun,
+  TextWrappingType,
+  TextWrappingSide,
+  Alignment,
+} from 'docx';
 @Component({
   selector: 'app-ms-schn-preview',
   templateUrl: './ms-schn-preview.component.html',
@@ -53,31 +65,199 @@ export class MsSchnPreviewComponent implements OnInit {
     });
   }
 
-  exportToWord() {
-    var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>lehrstell-motivation-sschreiben</title></head><body style='font-family: Calibri'>";
-    var postHtml = "</body></html>";
-    var html = preHtml + this.content.nativeElement.innerHTML + postHtml;
 
-    // Specify link url
-    var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
 
-    // Specify file name
-    let filename = 'lehrstell-motivation-sschreiben' + new Date().toUTCString() + '.doc'
-    // Create download link element
-    var downloadLink = document.createElement("a");
+ //sayem
+ exportToWord() {
+  const doc = new Document({
+    styles: {
+      default: {
+        heading2: {
+          run: {
+            size: 36,
+            bold: true,
+            color: '000000',
+            font: 'Calibri',
+          },
+          paragraph: {
+            spacing: {
+              after: 400,
+            },
+          },
+        },
+      },
+      paragraphStyles: [
+        {
+          id: 'paragraphStyle',
+          name: 'ParagraphStyle',
+          basedOn: 'Normal',
+          next: 'Normal',
+          run: {
+            color: '000000',
+            font: 'Calibri',
+          },
+        },
+      ],
+    },
+    title: 'lehrstell-motivation-sschreiben',
+    sections: [
+      {
+        properties: {
+          page: {
+            margin: {
+              top: 1500,
+              bottom: 1500,
+              left: 1500,
+              right: 1500,
+            },
+          },
+        },
+        children: [
+          // new Paragraph({
+          //     text: "Vorbereitungsfragen für dein Bewerbungsgespräch",
+          //     heading: HeadingLevel.HEADING_2
+          // }),
 
-    document.body.appendChild(downloadLink);
+          new Paragraph({
+            style: 'paragraphStyle',
+            spacing: {
+              before: 700,
+            },
+            children: [
+              // Md Sayem Hossain
+              // Middle hali street 10
+              // 4215 Chittagong
+              // my@email.com
+              // 018888888888
 
-    downloadLink.href = url;
+              // Md Sayem Hossain
+              new TextRun({
+                text: `${this.commonService.msPersonalForm.firstName}`,
+              }),
+              new TextRun({
+                text: ' ',
+              }),
+              new TextRun({
+                text: `${this.commonService.msPersonalForm.lastName}`,
+              }),
 
-    // Setting the file name
-    downloadLink.download = filename;
+              // Middle hali street 10
+              new TextRun({
+                break:1,
+                text: `${this.commonService.msPersonalForm.street}`,
+              }),
+              new TextRun({
+                text: ' ',
+              }),
+              new TextRun({
+                text: `${this.commonService.msPersonalForm.Number}`,
+              }),
+             
 
-    //triggering the function
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
 
-  }
+            // 4215 Chittagong
+            new TextRun({
+              break:1,
+              text: `${this.commonService.msPersonalForm.zip}`,
+            }),
+            new TextRun({
+              text: ' ',
+            }),
+            new TextRun({
+              text: `${this.commonService.msPersonalForm.place}`,
+            }),
+            
+            
+            // 4215 Chittagong
+            new TextRun({
+              break:1,
+              text: `${this.commonService.msPersonalForm.email}`,
+            }),
+            
+
+
+            // 4215 Chittagong
+            new TextRun({
+              break:1,
+              text: `${this.commonService.msPersonalForm.mobile}`,
+            }),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ],
+          }),
+        ],
+      },
+    ],
+  });
+
+  Packer.toBlob(doc).then((blob) => {
+    let url = window.URL.createObjectURL(blob);
+    let a = document.createElement('a');
+    document.body.appendChild(a);
+    a.setAttribute('style', 'display: none');
+    a.href = url;
+    a.download =
+      'lehrstell-motivation-sschreiben' + new Date().toUTCString() + '.doc';
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // exportToWord() {
+  //   var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>lehrstell-motivation-sschreiben</title></head><body style='font-family: Calibri'>";
+  //   var postHtml = "</body></html>";
+  //   var html = preHtml + this.content.nativeElement.innerHTML + postHtml;
+
+  //   // Specify link url
+  //   var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+
+  //   // Specify file name
+  //   let filename = 'lehrstell-motivation-sschreiben' + new Date().toUTCString() + '.doc'
+  //   // Create download link element
+  //   var downloadLink = document.createElement("a");
+
+  //   document.body.appendChild(downloadLink);
+
+  //   downloadLink.href = url;
+
+  //   // Setting the file name
+  //   downloadLink.download = filename;
+
+  //   //triggering the function
+  //   downloadLink.click();
+  //   document.body.removeChild(downloadLink);
+
+  // }
 
   isObjectEmpty(Obj: any) {
     for (var key in Obj) {
